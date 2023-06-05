52e008f10e7f9c3e5c87cafb4994ad975e16cf40
Awang Trisakti <trisaktiawang@gmail.com>
2023-06-05 21:00:03 +0700

    Fix migration change to golang migrate

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -22,7 +22,7 @@ func ConnectSql(driver, dbDsn string) *sql.DB {
 
 func GetDsn(driver, host, port, username, password, db string) string {
 	return map[string]string{
-		"mysql":    fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, db),
-		"postgres": fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", host, port, username, db, password),
+		"mysql":    fmt.Sprintf("mysql://%s:%s@tcp(%s:%s)/%s", username, password, host, port, db),
+		"postgres": fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=enable", username, password, host, port, db),
 	}[driver]
 }
